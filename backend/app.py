c6a9fea5497bd012ed419bd44c444f1dc75bbbe8
noureddine.bensadok@ouidou.fr <noureddine.bensadok@ouidou.fr>
2025-02-06 16:53:47 +0100

    backend

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
 from routes import csv_routes
-from asgiref.wsgi import WsgiToAsgi
+
 
 def create_app():
     app = Flask(__name__)
+
     CORS(app)
+
     app.register_blueprint(csv_routes, url_prefix='/api')
+
     return app
 
-# Create WSGI app
-wsgi_app = create_app()
-# Convert to ASGI app
-asgi_app = WsgiToAsgi(wsgi_app)
 
 if __name__ == '__main__':
-    wsgi_app.run(debug=True)
+    app = create_app()
+    app.run(debug=True)
